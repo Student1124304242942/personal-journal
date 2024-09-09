1f7a8983be68279bb2b083c1255ede781da3facf
Yunus936 <pulat2007@mail.ru>
2024-09-09 22:23:30 +0500

    second commit

--- src/components/hooks/use-localStorage.hook.js
+++ src/components/hooks/use-localStorage.hook.js
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
 
-export function useLocalStorage(key){
-    const [data, setData] = useState();
+export function useLocalStorage(key) {
+    const [data, setData] = useState(() => {
+        const storedData = localStorage.getItem(key);
+        return storedData ? JSON.parse(storedData) : []; // Инициализируйте пустым массивом, если нет данных
+    });
 
     useEffect(() => {
         const res = JSON.parse(localStorage.getItem(key));
-        if(res){
+        if (res) {
             setData(res);
         }
-      }, [key]);
+    }, [key]);
 
     const saveData = (newData) => {
-      localStorage.setItem(key, JSON.stringify(newData));
-      setData(newData);
+        localStorage.setItem(key, JSON.stringify(newData));
+        setData(newData);
     };
 
     return [data, saveData];
